0bd3c6e3e651e8e5c8cad9cd8d08d7cd7520c823
wanyifan <2392048412@qq.com>
2020-03-20 20:56:48 +0800

    login

--- app.js
+++ app.js
@@ -6,43 +6,91 @@ App({
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
 
+    this.login();
+    
+    
+  },
+  
+  login: function(){
+    console.log('login start');
     // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId oZ7iB4namu9ite1eeLdD9ZnkBtXM
-        console.log(res.code);
-        wx.request({
-          url: this.globalData.URL+"/user/login",
-          data: {
-            code:res.code
-          },
-          header: {'content-type': 'application/x-www-form-urlencoded'},
-          method: 'POST',
-          // dataType: 'json',
-          // responseType: 'text',
-          success: (result)=>{
-            console.log(result)
-          },
-          fail: ()=>{},
-          complete: ()=>{}
-        });
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
+    wx.checkSession({
+      success: (result) => {
+        console.log('存在登录态');
+        this.getUserInfo(this.globalData);
+      },
+      fail: () => {
+        // 登录
+        console.log('relogin');
+        wx.login({
+          success: res => {
+            // 发送 res.code 到后台换取 openId, sessionKey, unionId oZ7iB4namu9ite1eeLdD9ZnkBtXM
+            console.log(res.code);
+            wx.request({
+              url: this.globalData.URL + "/login",
+              data: {
+                code: res.code
+              },
+              header: { 'content-type': 'application/x-www-form-urlencoded' },
+              method: 'POST',
+              success: (result) => {
+                console.log("111" + JSON.stringify(result))
+                var data = result.data;
+                wx.setStorage({
+                  key: 'token',
+                  data: data.data,
+                  success: (result) => {
+                    console.log("存入token");
+                    this.getUserInfo(this.globalData);
+                  },
+                  fail: () => { }
+                });
+              },
+              fail: () => { },
+              complete: () => { }
+            });
+          }
+        })
+      },
+      complete: () => { }
+    });
 
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
+  },
+  getUserInfo: function (globalData) {
+    wx.getUserInfo({
+      success: res => {
+        if (res == null) {
+          console.log("no can get userInfo");
+          wx.switchTab({
+            url: '/pages/user/index',
+          })
+        } else {
+          var userInfo = res;
+          // console.log(userInfo);
+          wx.getStorage({
+            key: 'token',
+            success: function (res) {
+              console.log("-----" + JSON.stringify(userInfo));
+              wx.request({
+                url: globalData.URL + "/updateUserInfo",
+                data: {
+                  token: res.data,
+                  userInfo: JSON.stringify(userInfo)
+                },
+                header: { 'content-type': 'application/x-www-form-urlencoded' },
+                method: 'POST',
+                dataType: 'json',
+                responseType: 'text',
+                success: function (res) { 
+                  console.log(res);
+                },
+                fail: function (res) {
+                  console.log(res);
+                }
+              })
+            },
+            fail: function (res) {
+              console.log("fail");
             }
           })
         }
@@ -50,7 +98,8 @@ App({
     })
   },
   globalData: {
-    URL:'http://127.0.0.1:8080/api',
-    userInfo: null
+    URL: 'http://127.0.0.1:8081/api',
+    userInfo: null,
+    loginCode: null
   }
 })
